3f5ac3ec20947cdc3957ce5f3a3e89e7b79ed1f0
MaxPavliv <maxyxpavliv@gmail.com>
2023-09-16 23:13:05 +0300

    add start_activity.kt

---
+++ app/src/main/java/com/example/add_poduct/activity/start_activity.kt
@@ -0,0 +1,20 @@
+package com.example.add_poduct.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.add_poduct.databinding.ActivityCategotyChooseBinding
+import com.example.add_poduct.databinding.ActivityStartBinding
+
+class start_activity : AppCompatActivity() {
+    lateinit var binding: ActivityStartBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.addProduct.setOnClickListener {
+            val AddIntent= Intent(this,categoty_choose::class.java)
+            startActivity(AddIntent)
+        }
+    }
+}
